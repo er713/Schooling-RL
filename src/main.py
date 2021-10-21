6f0ddb5b0a0f2c4c56c57ae89466924c0bb33ea9
Piotr Linkowski <44367698+linek150@users.noreply.github.com>
2021-10-21 14:45:10 +0200

    Revert "Added missing rash_student.py"

--- src/main.py
+++ src/main.py
@@ -1,11 +1,6 @@
 """
 main file
 """
-from school import Classroom
-from school.teachers import Teacher
-from school.students import Student
 
-if __name__ == '__main__':
-    c = Classroom(7, Teacher, Student)
-    # c.run(10)
-    print(c)
+if __name__ =='__main__':
+    pass
--- src/school/students/__init__.py
+++
@@ -1,2 +0,0 @@
-__all__ = ["Student"]
-from .student import Student
