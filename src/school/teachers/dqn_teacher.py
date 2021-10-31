a021fe98a384648e6f5dd327f6ad6019cc9ec333
aizemski <aleksanderizemski@gmail.com>
2021-10-31 18:47:16 +0100

    Prepared dqn_teacher and dqn files

---
+++ src/school/teachers/dqn_teacher.py
@@ -0,0 +1,22 @@
+class Agent(object):
+    """Deep Q-learning agent."""
+
+    def __init__(self):
+        """Set parameters, initialize network."""
+        pass
+
+    def step(self, observation, training=True):
+
+        pass
+
+    def policy(self, state, training):
+        """Epsilon-greedy policy for training, greedy policy otherwise."""
+        pass
+
+    def update_target_network(self):
+        """Update target network weights with current online network values."""
+        pass
+
+    def train_network(self):
+        """Update online network weights."""
+        pass
