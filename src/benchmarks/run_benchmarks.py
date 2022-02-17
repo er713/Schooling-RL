112ff5b727948f4619e2b6efc0ae923fe2a9d573
Kamil Pluciński <kamil.plucinski@deepsense.ai>
2022-02-17 19:04:46 +0100

    add a2c

--- src/benchmarks/run_benchmarks.py
+++ src/benchmarks/run_benchmarks.py
@@ -7,31 +7,47 @@ from pytorch_lightning.loggers import WandbLogger
 
 from sklearn.model_selection import ParameterGrid
 
+from benchmarks.a2c import UpdatedA2C
+
 param_grid = {
     "skills_quantity": [1, 2, 5, 10],
-    "train_time_per_skill": [4, 8, 12, 16],
+    "train_time_per_skill": [6, 8, 10, 12],
     "seed": [1, 2, 3, 4, 5],
     "env_name": ["gradesbook-v0"],
     # "env_name": ["gradesbook-v0", "gradeslist-v0"],
-    "model": ["ac", "dqn"],
+    # "model": ["a2c", "dqn"],
+    "model": ["a2c"],
 }
-model_to_class = {"a2c": AdvantageActorCritic, "dqn": DQN}
-model_to_train_params = {"a2c": {}, "dqn": {}}
+# iterations ale calculated to make the same number of episodes -> 10k
+# episode len = training_time -> skills_quantity * train_time_per_skill
+
+model_to_class = {"a2c": UpdatedA2C, "dqn": DQN}
+model_to_train_params = {
+    "a2c": {
+        "batch_size": 256,
+        "epoch_len": 1,  # one batch so max epochs == 100 000 // 256
+    },
+    "dqn": {},
+}
+
+
 for params in ParameterGrid(param_grid):
+    episode_length = params["skills_quantity"] * params["train_time_per_skill"]
+
     gym.envs.register(
         id="gradesbook-v0",
-        entry_point="environment:GradesBookEnvironment",
+        entry_point="environment.environment:GradesBookEnvironment",
         kwargs={
             "skills_quantity": params["skills_quantity"],
-            "time_to_exam": params["skills_quantity"] * params["train_time_per_skill"],
+            "time_to_exam": episode_length,
         },
     )
     gym.envs.register(
         id="gradeslist-v0",
-        entry_point="environment:GradesListEnvironment",
+        entry_point="environment.environment:GradesListEnvironment",
         kwargs={
             "skills_quantity": params["skills_quantity"],
-            "time_to_exam": params["skills_quantity"] * params["train_time_per_skill"],
+            "time_to_exam": episode_length,
         },
     )
 
@@ -41,6 +57,12 @@ for params in ParameterGrid(param_grid):
 
     model = model_class(env=params["env_name"], **model_params)
     wandb_logger = WandbLogger(project="schooling-rl", name="benchmarking")
-    trainer = Trainer(deterministic=True, logger=None)
+
+    required_actions = episode_length * 20000
+    trainer = Trainer(
+        deterministic=True,
+        logger=None,
+        max_epochs=required_actions // model_params["batch_size"],
+    )
     trainer.fit(model)
     break
